1589a808b02a93ce152ef988c75857e4f66491b9
Amilton Brune da Silva Junior <amilton.junior@digisystem.com.br>
2019-09-04 19:33:35 -0300

    Correção e teste deploy gh-pages

---
+++ precache-manifest.3bfcf91929fec05760fb55d0dc800802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4217b6729c0a9a48ba0a29d7fb6958ce",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "50a5917153276cbd616b",
+    "url": "/react-calculator/static/css/main.9e40860f.chunk.css"
+  },
+  {
+    "revision": "e68e6a59e9ac64eee011",
+    "url": "/react-calculator/static/js/2.9a696b8f.chunk.js"
+  },
+  {
+    "revision": "50a5917153276cbd616b",
+    "url": "/react-calculator/static/js/main.a751195c.chunk.js"
+  },
+  {
+    "revision": "9d7eb8c3f20c96c79f97",
+    "url": "/react-calculator/static/js/runtime~main.afaf0077.js"
+  },
+  {
+    "revision": "0363f38fb5a18e29976b36537e97c2ea",
+    "url": "/react-calculator/static/media/robot-507811_1920.0363f38f.jpg"
+  }
+]);
