9f6b47773516c66a6fb9a6cbac2ac210d887a2ed
Amilton Brune da Silva Junior <amilton.junior@digisystem.com.br>
2019-09-04 17:49:28 -0300

    Updates

---
+++ precache-manifest.21e58e6fe2e686d8c564aea54291e70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51cd82978c0acc5501379e419859cb9f",
+    "url": "/AmiltonBrune/react-calculator/index.html"
+  },
+  {
+    "revision": "db6d325049a332918c6f",
+    "url": "/AmiltonBrune/react-calculator/static/css/main.12352963.chunk.css"
+  },
+  {
+    "revision": "e68e6a59e9ac64eee011",
+    "url": "/AmiltonBrune/react-calculator/static/js/2.9a696b8f.chunk.js"
+  },
+  {
+    "revision": "db6d325049a332918c6f",
+    "url": "/AmiltonBrune/react-calculator/static/js/main.a751195c.chunk.js"
+  },
+  {
+    "revision": "3e2a18938f099f98d69c",
+    "url": "/AmiltonBrune/react-calculator/static/js/runtime~main.34e38fb0.js"
+  },
+  {
+    "revision": "0363f38fb5a18e29976b36537e97c2ea",
+    "url": "/AmiltonBrune/react-calculator/static/media/robot-507811_1920.0363f38f.jpg"
+  }
+]);
