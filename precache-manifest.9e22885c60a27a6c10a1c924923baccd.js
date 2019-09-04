c86fd51da73dcac96314b2eae89f098604707081
Amilton Brune da Silva Junior <amilton.junior@digisystem.com.br>
2019-09-04 17:31:32 -0300

    Subindo build calculadora com react

---
+++ precache-manifest.9e22885c60a27a6c10a1c924923baccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008951a3b98f8170b099b05ffef14e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cb40d14255b84dd577c",
+    "url": "/static/css/main.085782af.chunk.css"
+  },
+  {
+    "revision": "e68e6a59e9ac64eee011",
+    "url": "/static/js/2.9a696b8f.chunk.js"
+  },
+  {
+    "revision": "3cb40d14255b84dd577c",
+    "url": "/static/js/main.a751195c.chunk.js"
+  },
+  {
+    "revision": "cdaa730cd4e5889b22cb",
+    "url": "/static/js/runtime~main.3a411527.js"
+  },
+  {
+    "revision": "0363f38fb5a18e29976b36537e97c2ea",
+    "url": "/static/media/robot-507811_1920.0363f38f.jpg"
+  }
+]);
